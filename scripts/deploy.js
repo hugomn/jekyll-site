4005f0672170f495744e7d7618588d521dafcab1
Hugo Nogueira <hugomn@gmail.com>
2018-08-09 15:39:36 +0200

    fix: Add author info

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,5 +3,9 @@ var ghpages = require('gh-pages');
 // TODO: improve output https://github.com/tschaub/gh-pages
 ghpages.publish('_site', {
     branch: 'master',
-    repo: 'git@github.com:hugomn/hugomn.github.io.git'
+    repo: 'git@github.com:hugomn/hugomn.github.io.git',
+    user: {
+        name: 'Hugo Nogueira',
+        email: 'hugomn@gmail.com'
+    }
   });
